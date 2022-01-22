723860abdc88e7ab12274b18051dc3e481e306ff
Chris Hammock <chris@crshmk.com>
2022-01-22 13:55:54 -0600

    basic jest setup

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+}
+
+module.exports = config
+
+module.exports = async () => {
+  return {
+    verbose: true,
+    testEnvironment: "jsdom",
+    preset: "./jest-preset.js",
+    moduleNameMapper: {
+      "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
+      "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/genericMock.js",
+
+    }
+  }
+}
