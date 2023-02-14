9eb2dad40206037e34b1b3ffa7b665d75fe60f99
Chris Hammock <chris@crshmk.com>
2023-02-14 20:54:26 +0700

    use template icons to accommodate dark/light screens

--- main/createTray/index.js
+++ main/createTray/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const { Tray } = require('electron')
 const { toggleWindow } = require('./toggleWindow')
 
-const iconPath = path.join(__dirname, './tray-icon.png')
+const iconPath = path.join(__dirname, './fuelpumpTemplate.png')
 
 const createTray = window => {
   const trayIcon = new Tray(iconPath)
