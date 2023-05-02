6208b84ffc504229527e155890037deccb33be19
Chris Hammock <chris@crshmk.com>
2023-05-02 18:52:42 +0700

    update root api for react 18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 
 import App from './App'
 
-const root = document.getElementsByTagName('div')[0]
+const container = document.getElementsByTagName('div')[0]
 
-ReactDOM.render(<App />, root)
+const root = createRoot(container)
+
+root.render(<App  />)
+
